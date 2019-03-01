c249ebfb64fdb837f65f1515fb6b26d78c826163
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2019-03-01 18:40:14 -0500

    Removed code from test.js and inserted code to preform an arbitrary while loop.

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 // Declares variable to store user's age.
-var myAge = 26;
+var age = 5;
 
-// Determines whether the user is the appropriate age or not.
-if (myAge < 18 || myAge > 30 || myAge === 25) {
-	document.write('you aint coming!');
-} else {
-	document.write('You can come, you cool dude');
+// If the user's age is less than 10 it logs a message in console repeatedly relative to the difference of the user's age to 10.
+while (age < 10) {
+	console.log('Your age is less than 10');
+	age++;
 }
+
+// Writes to the document once the user's age is incremented to 10.
+document.write('you are now over 10');
