000a88aa5405809c151ea512f8b271a84fc2b643
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2019-03-01 17:53:56 -0500

    Removed code from test.js and added code to check user's age and invites the user to come along with based on appropriate age.

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
 // Declares variable to store user's age.
-var myAge = 29;
+var myAge = 26;
 
-// Determines whether user is over 30 or over 20 or over 10 or under 10.
-if (myAge > 30) {
-	document.write('you are over 30!');
-} else if (myAge > 20){
-	document.write('you are over 20!');
-} else if (myAge > 10){
-	document.write('you are over 10!');
+// Determines whether the user is the appropriate age or not.
+if (myAge < 18 || myAge > 30 || myAge === 25) {
+	document.write('you aint coming!');
 } else {
-	document.write('you are not over 10!');
+	document.write('You can come, you cool dude');
 }
